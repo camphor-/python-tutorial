4f6955f68363ac5ae4af3abb0911d1137de638cf
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-02-22 00:31:06 +0900

    Add source

---
+++ source/3-input.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# ## input
+# [`input()`](http://docs.python.jp/3/library/functions.html#input) 関数は, 入力から1行を読み込み, その結果を文字列として返します.
+# 
+
+# ## 文字列の入力
+
+# In[1]:
+
+name = input("名前は?")
+print(name + "さん、こんにちは")
+
+
+# ## 整数の入力
+# `input()` 関数は入力された値を文字列として返します.
+# `int()` を利用すると, 文字列を整数に変換することが出来ます.
+# これらを組み合わせると, 入力された文字列を整数に変換して利用することができます.
+
+# In[2]:
+
+multiplier = input("2を何倍しますか?")
+multiplier = int(multiplier)  # 文字列 -> 整数の変換
+print(2 * multiplier)
+
+
+# ## 練習
+# - 2つの整数を入力して, その積 (乗算の結果) を表示するプログラムを作って実行しましょう
---
+++ source/index.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# # Python チュートリアル
+# プログラミング初心者のための, Python を用いたプログラミング入門.
+
+# ## この資料について
+# この資料はプログラミング初心者が, Python を用いてプログラミングに入門するためのものです.
+# 一人で学習するための資料ではなく, Python を知っている人と一緒に学習することを想定して記述されています.
+# このため Python の開発環境の構築についての記述はこの資料にはありません.
+
+# ## 環境について
+# この資料は Python 3 について記述しています. Python 2 では一部動作しない部分があります.
+
+# ## 目次
+# - [はじめに](0-introduction.html)
+# - [print](1-print.html)
+# - [変数](2-variables.html)
+# - [input](3-input.html)
+# - [関数](4-functions.html)
+# - [データ構造](5-data-structures.html)
+# - [制御構造](6-control-flow.html)
+# - [練習: 英単語ゲーム](7-exercise.html)
+
+# ## ライセンス
+# <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="クリエイティブ・コモンズ・ライセンス" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png"></a><br>この作品は <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">クリエイティブ・コモンズ 表示 - 非営利 4.0 国際 ライセンスの下に提供されています。</a>
+
+# ## 著者
+# ### 著者
+# - [@ymyzk](https://github.com/ymyzk)
+# 
+# ### レビュー
+# - [@tanishiking](https://github.com/tanishiking)
+# - [@yu-i9](https://github.com/yu-i9)
